3d3cf3ca7d926e83a38a8532a52e5faca78c17e8
calmen_curtin <20072144@student.curtin.edu.au>
2022-10-06 19:52:08 +0800

    Add Database Server

---
+++ DatabaseServer/Controllers/ClientsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DatabaseServer.Models;
+
+namespace DatabaseServer.Controllers
+{
+    [RoutePrefix("api/clients")]
+    public class ClientsController : ApiController
+    {
+        private PeerToPeerDatabaseEntities db = new PeerToPeerDatabaseEntities();
+
+        [Route("")]
+        public IQueryable<Client> GetClients()
+        {
+            return db.Clients;
+        }
+
+        [Route("{id}")]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClient(string id)
+        {
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        [Route("update/{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClient(string id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.IP_Address)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("add")]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clients.Add(client);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClientExists(client.IP_Address))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            CreatedAtRoute("DefaultApi", new { id = client.IP_Address }, client);
+            return Ok();
+        }
+
+        [Route("delete/{id}")]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult DeleteClient(string id)
+        {
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Clients.Remove(client);
+            db.SaveChanges();
+
+            return Ok(client);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(string id)
+        {
+            return db.Clients.Count(e => e.IP_Address == id) > 0;
+        }
+    }
+}
